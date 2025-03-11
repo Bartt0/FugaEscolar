00af4010654c8b3d1a89eae7e3d071040a0097f5
Bartt0 <fide_arias_zavala@hotmail.com>
2025-03-11 08:28:18 -0600

    -pantalla abierta

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
-    
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type ==  pygame.QUIT:
+            running = False
+
+
 screen.fill("purple")
 
 
 pygame.display.flip()
 
-clock.tick(1)  
+clock.tick()  
 
-pygame.quit()
+pygame.quit(60)
